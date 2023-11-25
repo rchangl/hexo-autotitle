bb35a8fac2dcbf81ead1a2711c507f5048b50909
rchangl <rchangl@outlook.com>
2023-11-25 21:58:39 +0800

    1.0.1 bug fix

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 'use strict';
 
 hexo.extend.filter.register('before_post_render', (data) => {
-    if (data.title != null) {
-        
+    // console.log("Plugin is running.");
+    if (data.title === null
+        || data.title == "") {
+
         // 匹配 "# (title)"
         let titleExp = /^#\x20+(.+)/m;
         let title = data.content.match(titleExp);
 
         if (!(title === null)) {
             data.title = title[1];
-            console.log(data.source + " 标题为：" + title[1]);
+            console.log(data.source + " Title found: " + title[1]);
         } else {
-            console.log(data.source + " 标题为空");
+            console.log(data.source + " Title is null");
         }
-    }
+    } 
+    // else if (data.title == null) {
+    //     console.log("data.title == " + data.title);
+    // } else {
+    //     console.log("[have title] 类型：" + typeof data.title + "值：" + data.title);
+    // }
 });
